7de2d60759af46cd300c5c9c903027a7837ecb03
erhayer <w213862@gmail.com>
2024-09-09 20:05:57 +0900

    Title: 정수 입력받아 계산, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240909/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        System.out.println(a*2+3);
+    }
+}
