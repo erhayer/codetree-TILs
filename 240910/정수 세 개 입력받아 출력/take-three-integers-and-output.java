bb091721b05d5ea1d7c519c1b36bcb9c873e6995
erhayer <w213862@gmail.com>
2024-09-10 18:33:48 +0900

    Title: 정수 세 개 입력받아 출력, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240910/정수 세 개 입력받아 출력/take-three-integers-and-output.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(a).append(" ").append(b).append(" ").append(c);
+        System.out.println(sb);
+    }
+}
