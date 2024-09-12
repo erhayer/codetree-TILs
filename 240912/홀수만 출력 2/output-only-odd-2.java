0b70a6665b50e258df8afe4035b36f1a56406c84
erhayer <w213862@gmail.com>
2024-09-12 20:00:06 +0900

    Title: 홀수만 출력 2, Time: 106ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240912/홀수만 출력 2/output-only-odd-2.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        for(int i = a; i>=b; i -= 2){
+            System.out.print(i+" ");
+        }
+    }
+}
