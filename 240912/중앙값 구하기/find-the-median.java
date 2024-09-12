79230635bf5636d09b59a5f5d49ff8b4486f73a6
erhayer <w213862@gmail.com>
2024-09-12 19:53:29 +0900

    Title: 중앙값 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240912/중앙값 구하기/find-the-median.java
@@ -0,0 +1,27 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if (a>b){
+            if (a>c){
+                System.out.println(c);
+            } else {
+                System.out.println(a);
+            }
+        } else {
+            if (c>b){
+                System.out.println(b);
+            } else {
+                System.out.println(a>c?a:c);
+            }
+        }
+    }
+}
