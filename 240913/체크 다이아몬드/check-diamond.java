57e536fc0641eed1f7c9327e36edb278a4476bd5
erhayer <w213862@gmail.com>
2024-09-13 18:19:23 +0900

    Title: 체크 다이아몬드, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240913/체크 다이아몬드/check-diamond.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        boolean ind = true;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < 2*i+1; j++) {
+                if (ind){
+                    sb.append("*");
+                } else {
+                    sb.append(" ");
+                }
+                ind = !ind;
+            }
+            ind = true;
+            sb.append("\n\n");
+        }
+
+        for (int i = n-2; i >= 0; i--) {
+            for (int j = 0; j < n-i-1; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < 2*i+1; j++) {
+                if (ind){
+                    sb.append("*");
+                } else {
+                    sb.append(" ");
+                }
+                ind = !ind;
+            }
+            ind = true;
+            sb.append("\n\n");
+        }
+
+        System.out.println(sb);
+    }
+}
