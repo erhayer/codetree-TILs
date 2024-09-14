551cbb8ec5c455ee73ec50f5d5d8a642c317e78f
erhayer <w213862@gmail.com>
2024-09-14 19:22:25 +0900

    Title: 짝수만 거꾸로 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240914/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = (Integer.parseInt(br.readLine()));
+        }
+        int temp;
+        for (int i = n-1; i >= 0; i--){
+            temp = arr[i];
+            if (temp%2==0){
+                System.out.println(temp + " ");    
+            }
+                
+        }
+        
+        
+    }
+
+}
