b6e3b974a50d6057d1507e4da475955dafb46130
erhayer <w213862@gmail.com>
2024-09-21 14:27:10 +0900

    Title: 함수를 이용한 윤년 판별, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240921/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int y = sc.nextInt();
+        if (isLeapYear(y)){
+            System.out.println("true");
+        } else{
+            System.out.println("false");
+        }
+    }
+    public static boolean isLeapYear(int y){
+        if (y%100==0 && y%400!=0) return false;
+        if (y%4==0) return true;
+        return false;
+    }
+}
