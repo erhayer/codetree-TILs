81d9bbecda62fbccfef397ec3dd5d360154f23b3
erhayer <w213862@gmail.com>
2024-09-21 14:10:03 +0900

    Title: 함수를 이용한 소수 판별, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240921/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 240921/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -14,6 +14,7 @@ public class Main {
         System.out.println(sum);
     }
     public static boolean isPrime(int n){
+        if (n == 1) return false;
         for (int i = 2; i < n/2; i++){
             if (n%i==0) return false;
         }
