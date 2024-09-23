e99d073dca5237cbdafca87bc53ea1bcc34fd3e5
erhayer <w213862@gmail.com>
2024-09-23 20:07:29 +0900

    Title: 2021년 날짜의 유무, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240923/2021년 날짜의 유무/with-or-without-2021.java
+++ 240923/2021년 날짜의 유무/with-or-without-2021.java
@@ -16,14 +16,13 @@ public class Main {
     public static boolean mdExists(int m, int d){
         if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12){
             if (d <= 31 && d > 0) return true;
-            return false;
         }
         if (m == 2){
             if (d <= 28 && d > 0) return true;
-            return false;
-        } else{
-            if (d <= 30 && d > 0) return true;
-            return false;
         }
+        if (m == 2 || m == 4 || m == 6 || m == 9 || m == 11){
+            if (d <= 30 && d > 0) return true;
+        }
+        return false;
     }
 }
