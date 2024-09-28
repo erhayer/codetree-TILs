36ec22ad2b7ffe6c91192095668cd0b373a1d3fa
erhayer <w213862@gmail.com>
2024-09-28 18:41:40 +0900

    Title: 문자열 정렬, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240928/문자열 정렬/string-sort.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] arr = new char[str.length()];
+        for (int i = 0; i < str.length(); i++){
+            arr[i] = str.charAt(i);
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < str.length(); i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
