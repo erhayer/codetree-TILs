14aeacf555f330dc41998d9fab40ef3a017ef40c
erhayer <w213862@gmail.com>
2024-10-04 19:34:28 +0900

    Title: 최대로 겹치는 지점, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 241004/최대로 겹치는 지점/maximum-overlapped-points.java
+++ 241004/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -14,8 +14,8 @@ public class Main {
 
         int x1, x2;
         for (int i = 0; i < n; i++){
-            x1 = sc.nextInt() + 100;
-            x2 = sc.nextInt() + 100;
+            x1 = sc.nextInt();
+            x2 = sc.nextInt();
 
             for (int j = x1; j <= x2; j++) arr[j]++;
         }
