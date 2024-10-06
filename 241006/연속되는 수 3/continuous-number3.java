68d5d2258db4f24ceb0727ff79a0391caf6eeebb
erhayer <w213862@gmail.com>
2024-10-06 14:19:51 +0900

    Title: 연속되는 수 3, Time: 186ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241006/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+
+        int max = 0, count = 0;
+        for (int i = 0; i < n; i++){
+            if (i == 0 || arr[i-1]*arr[i] < 0){
+                count = 1;
+            } else count++;
+            if (count>max) max = count;
+        }
+
+        System.out.println(max);
+    }
+}
