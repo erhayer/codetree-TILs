56552fca7fe3dd40edc616b2b5d13480a6441e41
erhayer <w213862@gmail.com>
2024-10-07 19:18:26 +0900

    Title: 선두를 지켜라, Time: 256ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 241007/선두를 지켜라/keep-the-lead.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Main {
+    public static final int MAX_TIME = 1000000;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] a = new int[MAX_TIME];
+        int[] b = new int[MAX_TIME];
+        for (int i = 0; i < MAX_TIME; i++){
+            a[i] = 0;
+            b[i] = 0;
+        }
+
+        int index_a = 0;
+        int index_b = 0;
+        int pos_a = 0;
+        int pos_b = 0;
+        
+        int v, t;
+
+        for (int i = 0; i < n; i++){
+            v = sc.nextInt();
+            t = sc.nextInt();
+
+            for (int j = 0; j < t; j++){
+                pos_a += v;
+                a[++index_a] = pos_a;
+            }
+        }
+
+        for (int i = 0; i < m; i++){
+            v = sc.nextInt();
+            t = sc.nextInt();
+
+            for (int j = 0; j < t; j++){
+                pos_b += v;
+                b[++index_b] = pos_b;
+            }
+        }
+
+        int[] diff = new int[index_a+1];
+        for (int i = 1; i <= index_a; i++){
+            diff[i] = a[i] - b[i];
+        }
+
+        int temp = 0;
+        int cnt = 0;
+        for (int i = 1; i <= index_a; i++){
+            if (diff[i]==0) continue;
+            if (temp == 0) temp = diff[i];
+            if (temp*diff[i] < 0){
+                cnt++;
+                temp = diff[i];
+            }
+        }
+
+        System.out.println(cnt);
+
+    }
+}
