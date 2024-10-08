9902e4ce106647ccbbc6a0fbd6646121f4c17e39
erhayer <w213862@gmail.com>
2024-10-08 20:01:41 +0900

    Title: 선두를 지켜라 3, Time: 226ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 241008/선두를 지켜라 3/keep-the-lead-3.java
+++ 241008/선두를 지켜라 3/keep-the-lead-3.java
@@ -10,6 +10,8 @@ public class Main{
 
         int[] a = new int[MAX_T+1];
         int[] b = new int[MAX_T+1];
+        a[0] = 0;
+        b[0] = 0;
         int t_a = 0;
         int t_b = 0;
 
@@ -31,14 +33,15 @@ public class Main{
             }
         }
 
-        int aCompareToB = compareAB(a[1], b[1]);
+        int aCompareToB = 0;
         int cnt = 0;
         int temp;
-        for (int i = 2; i <= t_a; i++){
+        for (int i = 1; i <= t_a; i++){
             temp = compareAB(a[i], b[i]);
             if (aCompareToB != temp) {
                 cnt += 2;
                 if (aCompareToB == 0 || temp == 0) cnt--;
+                aCompareToB = temp;
             }
         }
 
