17a06985944523ceebd94a4fc95f5c7a784df59c
erhayer <w213862@gmail.com>
2024-10-13 19:53:46 +0900

    Title: 바구니 안의 사탕 2, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

--- 241013/바구니 안의 사탕 2/candy-in-the-basket-2.java
+++ 241013/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -13,6 +13,9 @@ public class Main {
 
         int x;
         int num;
+        for (int i = 0; i < MAX_X; i++){
+            arr[i] = 0;
+        }
         for (int i = 0; i < n; i++){
             num = sc.nextInt();
             x = sc.nextInt();
@@ -22,13 +25,21 @@ public class Main {
 
         int max = 0;
         int temp;
-        for (int i = k; i < MAX_X - k; i++){
+        if (k >= MAX_X/2){
             temp = 0;
-            for (int j = i - k; j <= i + k; j++) {
-                temp += arr[j];
+            for (int i = 0; i < MAX_X; i++){
+                temp += arr[i];
             }
+            max = temp;
+        } else {
+            for (int i = k; i < MAX_X - k; i++){
+                temp = 0;
+                for (int j = i - k; j <= i + k; j++) {
+                    temp += arr[j];
+                }
 
-            max = Math.max(temp, max);
+                max = Math.max(temp, max);
+            }
         }
 
         System.out.println(max);
