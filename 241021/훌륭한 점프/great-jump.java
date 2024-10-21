ab24cd5774c91651183216a34124eb917cfb94dc
erhayer <w213862@gmail.com>
2024-10-21 19:16:37 +0900

    Title: 훌륭한 점프, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241021/훌륭한 점프/great-jump.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int dist;
+        boolean isMin = false;
+        for (int i = 100; i >= arr[0]; i--) {
+            dist = 0;
+            for (int j = 1; j < n; j++) {
+                dist++;
+
+                if (arr[j] <= i) {
+                    if (dist > k) {
+                        isMin = true;
+                        break;
+                    }
+                    dist = 0;
+                }
+            }
+
+            if (isMin) {
+                System.out.println(i+1);
+                break;
+            }
+        }
+    }
+}
