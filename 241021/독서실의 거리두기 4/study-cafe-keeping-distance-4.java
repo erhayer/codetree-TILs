52b643a774334e2ca2039e2c7324c33e4585556b
erhayer <w213862@gmail.com>
2024-10-21 19:55:43 +0900

    Title: 독서실의 거리두기 4, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 241021/독서실의 거리두기 4/study-cafe-keeping-distance-4.java
+++ 241021/독서실의 거리두기 4/study-cafe-keeping-distance-4.java
@@ -13,9 +13,9 @@ public class Main {
         }
         
         int max = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n-1; i++) {
             if (arr[i] == 1) continue;
-            for (int j = 0; j < n; j++) {
+            for (int j = i+1; j < n; j++) {
                 if (arr[j] == 1) continue;
 
                 arr[i] = 1;
