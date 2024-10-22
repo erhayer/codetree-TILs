d6e8b033d3ad15d8a7d88f0b7528a57ef1847cdc
erhayer <w213862@gmail.com>
2024-10-22 20:00:15 +0900

    Title: 두 선분, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241022/두 선분/two-lines.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x1 =sc.nextInt();
+        int x2 =sc.nextInt();
+        int x3 =sc.nextInt();
+        int x4 =sc.nextInt();
+
+        if (x3 > x2 || x1 > x4) {
+            System.out.println("nonintersecting");
+        } else {
+            System.out.println("intersecting");
+        }
+    }
+}
