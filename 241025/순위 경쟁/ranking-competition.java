8ae607af49eeda8da293998b22dc489a2fcfd660
erhayer <w213862@gmail.com>
2024-10-25 19:40:00 +0900

    Title: 순위 경쟁, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241025/순위 경쟁/ranking-competition.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int a = 0;
+        int b = 0;
+        int c = 0;
+
+        int tempA = 0;
+        int tempB = 0;
+        int tempC = 0;
+        int score;
+        char user;
+
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            user = sc.next().charAt(0);
+            score = sc.nextInt();
+
+            if (user == 'A') {
+                tempA += score;
+            } else if (user == 'B') {
+                tempB += score;
+            } else {
+                tempC += score;
+            }
+            
+            if (status(a, b, c) != status(tempA, tempB, tempC)) {
+                cnt++;
+            }
+
+            a = tempA;
+            b = tempB;
+            c = tempC;
+        }
+
+        System.out.println(cnt);
+    }
+
+    public static int status(int a, int b, int c) {
+        if (a == b && b == c) {
+            return 0;
+        } else if (a == b) {
+            return 1;
+        } else if (b == c) {
+            return 2;
+        } else if (c == a) {
+            return 3;
+        } else if (a > b && a > c) {
+            return 4;
+        } else if (b > a && b > c) {
+            return 5;
+        } else {
+            return 6;
+        }
+    }
+}
