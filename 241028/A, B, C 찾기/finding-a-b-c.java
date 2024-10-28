ec3d0bf5032e633a2b69510b3ada788765a2f87b
erhayer <w213862@gmail.com>
2024-10-28 20:03:57 +0900

    Title: A, B, C 찾기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241028/A, B, C 찾기/finding-a-b-c.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[7];
+
+        for (int i = 0; i < 7; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[0] + " " + arr[1] + " " + (arr[6] - arr[0] - arr[1]));
+    }
+}
