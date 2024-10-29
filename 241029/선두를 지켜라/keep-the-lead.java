31510013581645be2f1843c522a4bf0e495ad757
erhayer <w213862@gmail.com>
2024-10-29 18:33:56 +0900

    Title: 선두를 지켜라, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241029/선두를 지켜라/keep-the-lead.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int posA = 0;
+        int posB = 0;
+        int timeA = 0;
+        int timeB = 0;
+        int[] timelineA = new int[1000];
+        int[] timelineB = new int[1000];
+
+        int v, t;
+        for (int i = 0; i < n; i++) {
+            v = sc.nextInt();
+            t = sc.nextInt();
+
+            for (int j = 0; j < t; j++) {
+                posA += v;
+                timeA++;
+
+                timelineA[timeA] = posA;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            v = sc.nextInt();
+            t = sc.nextInt();
+
+            for (int j = 0; j < t; j++) {
+                posB += v;
+                timeB++;
+
+                timelineB[timeB] = posB;
+            }
+        }
+
+        int status = 0;
+        int tempStatus;
+        int cnt = 0;
+        for (int i = 1; i <= timeA; i++) {
+            tempStatus = timelineA[i] - timelineB[i];
+            if (status == 0) {
+                status = tempStatus;
+            } else if (status * tempStatus < 0) {
+                cnt++;
+                status = tempStatus;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
