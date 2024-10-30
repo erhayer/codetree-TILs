96995aee4150ed19448f2e77e8d33af8f8990bcf
erhayer <w213862@gmail.com>
2024-10-30 19:17:45 +0900

    Title: 황금비율 토스트, Time: 3075ms, Memory: 36MB, Status: Time Limit Exceed - CodeTree

---
+++ 241030/황금비율 토스트/golden-toast.java
@@ -0,0 +1,46 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        String str = sc.next();
+
+        LinkedList<Character> breads = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            breads.add(str.charAt(i));
+        }
+        ListIterator<Character> pos = breads.listIterator(n);
+
+        char inst;
+        for (int i = 0; i < m; i++) {
+            inst = sc.next().charAt(0);
+
+            if (inst == 'L') {
+                if (pos.hasPrevious()) {
+                    pos.previous();
+                }
+            } else if (inst == 'R') {
+                if (pos.hasNext()) {
+                    pos.next();
+                }
+            } else if (inst == 'P') {
+                pos.add(sc.next().charAt(0));
+            } else {
+                if (pos.hasNext()) {
+                    pos.next();
+                    pos.remove();
+                }
+            }
+        }
+
+        for (int i = 0; i < breads.size(); i++) {
+            System.out.print(breads.get(i));
+        }
+    }
+}
