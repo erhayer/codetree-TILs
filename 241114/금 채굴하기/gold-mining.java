19435dd4cfce36a073940d1467e59cc91d950c99
erhayer <w213862@gmail.com>
2024-11-14 18:57:44 +0900

    Title: 금 채굴하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 241114/금 채굴하기/gold-mining.java
+++ 241114/금 채굴하기/gold-mining.java
@@ -17,7 +17,7 @@ public class Main {
 
         int max = 0;
 
-        for (int k = 0; k < n; k++) {
+        for (int k = 0; k <= n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
                     max = Math.max(mineGold(mat, i, j, k, n, m), max);
