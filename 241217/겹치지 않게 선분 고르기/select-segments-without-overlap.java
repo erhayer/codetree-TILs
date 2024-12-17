fedde2b5219b71813cc9b0846b7b0bdc3fec238d
erhayer <w213862@gmail.com>
2024-12-17 19:26:08 +0900

    Title: 겹치지 않게 선분 고르기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241217/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int n;
+    public static int[][] lines;
+    
+    public static final int MAX = 1000;
+    public static int[] grid = new int[MAX+1];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        lines = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            lines[i] = new int[]{sc.nextInt(), sc.nextInt()};
+        }
+
+        System.out.println(countNotOverlappingLines(0, 0));
+    }
+
+    public static int countNotOverlappingLines(int index, int count) {
+        if (index >= n) {
+            return count;
+        }
+
+        int cnt = count;
+
+        boolean isOverlapping = false;
+        for (int i = lines[index][0]; i <= lines[index][1]; i++) {
+            if (grid[i] > 0) {
+                isOverlapping = true;
+                break;
+            }
+        }
+        
+        if (!isOverlapping) {
+            for (int i = lines[index][0]; i <= lines[index][1]; i++) {
+                grid[i] = 1;
+            }
+
+            cnt = Math.max(countNotOverlappingLines(index + 1, count+1), cnt);
+
+            for (int i = lines[index][0]; i <= lines[index][1]; i++) {
+                grid[i] = 0;
+            }
+        }
+
+        cnt = Math.max(countNotOverlappingLines(index + 1, count), cnt);
+
+        return cnt;
+    }
+}
