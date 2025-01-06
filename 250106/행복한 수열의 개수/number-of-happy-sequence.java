08f44ff4076cc24ef0ba388f8e38c087fcdeff79
erhayer <w213862@gmail.com>
2025-01-06 18:41:43 +0900

    Title: 행복한 수열의 개수, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250106/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] grid = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            //find happy row
+
+            int currR = grid[i][0];
+            int cntR = 1;
+
+            for (int j = 1; j < n; j++) {
+                int r = grid[i][j];
+
+                if (r == currR) {
+                    cntR++;
+                } else {
+                    currR = r;
+                    cntR = 1;
+                }
+
+                if (cntR >= m) {
+                    cnt++;
+                    break;
+                }
+            }
+
+            //find happy col
+
+            int currC = grid[0][i];
+            int cntC = 1;
+
+            for (int j = 1; j < n; j++) {
+                int c = grid[j][i];
+
+                if (c == currC) {
+                    cntC++;
+                } else {
+                    currC = c;
+                    cntC = 1;
+                }
+
+                if (cntC >= m) {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
