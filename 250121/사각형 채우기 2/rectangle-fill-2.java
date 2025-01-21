24d424b6031223d0f2cd7302ecea265b45b59499
erhayer <w213862@gmail.com>
2025-01-21 19:35:17 +0900

    Title: 사각형 채우기 2, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 250121/사각형 채우기 2/rectangle-fill-2.java
+++ 250121/사각형 채우기 2/rectangle-fill-2.java
@@ -11,7 +11,7 @@ public class Main {
         dp[1] = 1;
 
         for (int i = 2; i <= MAX_N; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2] * 2;
+            dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
         }
 
         System.out.println(dp[sc.nextInt()]);
