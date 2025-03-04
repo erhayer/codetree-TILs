4879635858f7d79a3506c75d05cc06d46bcc47f6
erhayer <w213862@gmail.com>
2025-03-04 21:03:00 +0900

    Title: 최대 합 분할, Time: 222ms, Memory: 20MB, Status: Passed - CodeTree

--- 250304/최대 합 분할/maximum-sum-partition.java
+++ 250304/최대 합 분할/maximum-sum-partition.java
@@ -6,34 +6,39 @@ public class Main {
         int[] arr = new int[n + 1];
         
         int m = 0;
+
         for(int i = 1; i <= n; i++){
             arr[i] = sc.nextInt();
             m += arr[i];
         }
+
+        int offset = m;
         
-        boolean[][] dp = new boolean[m + 1][m + 1];
-        dp[0][0] = true;
+        int[] dp = new int[2 * m + 1];
+        for (int i = -m; i <= m; i++) {
+            dp[i + offset] = -1;
+        }
+        dp[0 + offset] = 0;
 
         for (int i = 1; i <= n; i++) {
             int num = arr[i];
+            int[] nextDp = new int[2 * m + 1];
+            for (int j = -m; j <= m; j++) {
+                nextDp[j + offset] = dp[j + offset];
+            }
 
-            for (int j = m; j >= 0; j--) {
-                for (int k = m; k >= 0; k--) {
-                    if (dp[j][k]) {
-                        dp[j + num][k] = true;
-                        dp[j][num + k] = true;
-                    }
+            for (int j = -m; j <= m; j++) {
+                if (dp[j + offset] != -1) {
+                    nextDp[j - num + offset] = Math.max(dp[j + offset], nextDp[j - num + offset]);
+                    nextDp[j + num + offset] = Math.max(dp[j + offset] + num, nextDp[j + num + offset]);
                 }
             }
-        }
 
-        int max = 0;
-        for (int i = 0; i <= m; i++) {
-            if (dp[i][i]) {
-                max = i;
+            for (int j = -m; j <= m; j++) {
+                dp[j + offset] = nextDp[j + offset];
             }
         }
 
-        System.out.println(max);
+        System.out.println(dp[0 + offset]);
     }
 }
