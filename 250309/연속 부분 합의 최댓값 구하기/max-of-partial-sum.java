906021bd39395f7553bcf7208843d4d7094b2312
erhayer <w213862@gmail.com>
2025-03-09 18:32:02 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250309/연속 부분 합의 최댓값 구하기/max-of-partial-sum.java
+++ 250309/연속 부분 합의 최댓값 구하기/max-of-partial-sum.java
@@ -11,7 +11,7 @@ public class Main {
 
         int[] dp = new int[n];
 
-        dp[0] = nums[0] * 2;
+        dp[0] = nums[0];
 
         for (int i = 1; i < n; i++) {
             dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
