eadb161d2f2531902e24fce9038ef77c8a39d778
erhayer <w213862@gmail.com>
2025-03-09 18:19:52 +0900

    Title: 동일하게 분할, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250309/동일하게 분할/equal-partition.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n + 1];
+        boolean[] dp;
+
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        dp = new boolean[sum + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (dp[j]) {
+                    dp[j + arr[i]] = true;
+                }
+            }
+        }
+
+        if (sum % 2 == 0 && dp[sum / 2] == true) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
